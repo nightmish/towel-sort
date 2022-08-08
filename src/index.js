de364db80878ca48bac9b68a854c6cd29fcdffcb
Mikhail Tetiukhin <nightmish@gmail.com>
2022-08-08 15:23:54 +0300

    feat:working fuction towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,46 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let m = undefined;
+    if (matrix == undefined) {
+        m = [];    
+        }
+    else {
+    if (matrix.length === 2) {
+    matrix[1].reverse()
+    m = (matrix.join()).split(",")   
+    }
+    else if (matrix.length === 3) {
+    matrix[1].reverse()
+    m = (matrix.join()).split(",")   
+    }
+    else if (matrix.length === 4) {
+    matrix[1].reverse();
+    matrix[3].reverse();
+    m = (matrix.join()).split(",")   
+    }
+    else if (matrix.length === 5) {
+    matrix[1].reverse();
+    matrix[3].reverse(); 
+    m = (matrix.join()).split(",")   
+    }
+    else if (matrix.length === 6) {
+    matrix[1].reverse();
+    matrix[3].reverse();
+    matrix[5].reverse();
+    m = (matrix.join()).split(",")   
+    }
+    else if (matrix.length === 7) {
+    matrix[1].reverse();
+    matrix[3].reverse();
+    matrix[5].reverse();
+    m = (matrix.join()).split(",")   
+    }
+    else if (matrix.length === 0) {
+    m = [] 
+    };
 }
+return m;
+
+};
+
